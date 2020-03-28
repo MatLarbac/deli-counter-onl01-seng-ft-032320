4aac66cd85577d3ec8d496e0b08d74dac48a81eb
Mat Cabral <mathewcabral@gmail.com>
2020-03-28 03:12:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+     new_arr = []
+     katz_deli.each_with_index {|person, index| new_arr << "#{index+1}. #{person}"}
+     puts "The line is currently: #{new_arr.join(' ')}"
+end
+end
+
+def take_a_number(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  
